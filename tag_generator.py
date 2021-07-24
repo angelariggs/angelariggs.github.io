042afe0b6274d4c44fe395a8281ffa739027cdbd
Angela Riggs <riggs.ang@gmail.com>
2021-07-24 14:29:13 -0500

    removed extraneous archive page

--- tag_generator.py
+++ tag_generator.py
@@ -50,7 +50,7 @@ if not os.path.exists(tag_dir):
 for tag in total_tags:
     tag_filename = tag_dir + tag + '.md'
     f = open(tag_filename, 'a')
-    write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n' + "{% include archive.html %}"
+    write_str = '---\nlayout: tagpage\ntitle: \"Tag: ' + tag + '\"\ntag: ' + tag + '\nrobots: noindex\n---\n'
     f.write(write_str)
     f.close()
 print("Tags generated, count", total_tags.__len__())
